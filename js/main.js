9c4ca972f80c0019c3727e4e234d0d966936617e
Stanisław Dymny <dymnystan@gmail.com>
2023-09-10 10:42:55 +0200

    Code formated & fixes

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ const GenerateID = function (existingIDs = []) {
   return id;
 };
 
-const daysInMonth = (year,month) => new Date(year,month + 1,0).getDate();
+const daysInMonth = (year, month) => new Date(year, month + 1, 0).getDate();
 
 const calculateDateDiff = function (date1, date2 = new Date()) {
   const dateObj1 = {
@@ -47,45 +47,46 @@ const calculateDateDiff = function (date1, date2 = new Date()) {
     years: Math.abs(dateObj1.year - dateObj2.year),
     months: Math.abs(dateObj1.month - dateObj2.month),
   };
-  if(dateObj1.year === dateObj2.year && dateObj1.month === dateObj2.month)
-  {
+  if (dateObj1.year === dateObj2.year && dateObj1.month === dateObj2.month) {
     dateDiff.days = Math.abs(dateObj1.day - dateObj2.day);
+  } else {
+    dateDiff.days =
+      daysInMonth(dateObj1.year, dateObj1.month) -
+      Math.abs(dateObj1.day - dateObj2.day);
   }
-  else
-  {
-   dateDiff.days = daysInMonth(dateObj1.year,dateObj1.month) - Math.abs(dateObj1.day - dateObj2.day);
-  }
-  dateDiff.past = dateDiff.days + dateDiff.months + dateDiff.years && date1.getTime() < date2.getTime();
+  dateDiff.past =
+    dateDiff.days + dateDiff.months + dateDiff.years &&
+    date1.getTime() < date2.getTime();
   return dateDiff;
 };
 
-const dateString = function(dateDiff,date){
-  if (dateDiff.years + dateDiff.months + dateDiff.days === 0)
-  {
-    return 'Today';
-  }
-  else if (dateDiff.years <= 1 && dateDiff.months <= 1 && dateDiff.days <= 30)
-  {
-     return `${dateDiff.days} ${dateDiff.days > 1 ? 'days' : 'day'}`;
-  }
-  else if (dateDiff.years <= 1 && dateDiff.days === 0)
-  {
-    return `${dateDiff.months} ${dateDiff.months > 1 ? 'months' : 'month'}`
-  }
-  else {
+const dateString = function (dateDiff, date) {
+  if (dateDiff.years + dateDiff.months + dateDiff.days === 0) {
+    return "Today";
+  } else if (
+    dateDiff.years <= 1 &&
+    dateDiff.months <= 1 &&
+    dateDiff.days <= 30
+  ) {
+    return `${dateDiff.days} ${dateDiff.days > 1 ? "days" : "day"}`;
+  } else if (dateDiff.years <= 1 && dateDiff.days === 0) {
+    return `${dateDiff.months} ${dateDiff.months > 1 ? "months" : "month"}`;
+  } else {
     return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
   }
-}
+};
 const refreshList = function () {
   toDoListEl.innerHTML = "";
   toDoList.forEach(function (task) {
     const dateDiff = task.date && calculateDateDiff(task.date);
-    
+
     // !(Object.values(dateDiff).reduce((dateNumSum,dateNum) => dateNumSum+dateNum));
     const taskTemplate = `
     <div class="task" id="${task.id}">
       <h3 class="task__title" >${task.name}</h3>
-      <p class="task__date ${dateDiff.past ? 'task__date--past' : ''}" >${task.date && dateString(dateDiff,task.date)} ${ dateDiff.past ? 'ago' : ''}</p>
+      <p class="task__date ${dateDiff.past ? "task__date--past" : ""}" >${
+      task.date && dateString(dateDiff, task.date)
+    } ${dateDiff.past ? "ago" : ""}</p>
       <button class="task__delete-btn" id="delBtn-${task.id}">
       <i class="fa-solid fa-trash task__delete" style="color: #cc0000"></i>
       </button>
@@ -100,31 +101,30 @@ const refreshList = function () {
     const thisTaskID = document.getElementById(task.id);
 
     const delButton = document.getElementById(`delBtn-${task.id}`);
-    const editBtn = thisTaskID.querySelector('.task__edit');
+    const editBtn = thisTaskID.querySelector(".task__edit");
 
-    const descritpionEl = thisTaskID.querySelector('.task__description');
+    const descritpionEl = thisTaskID.querySelector(".task__description");
 
-    if (descritpionEl.scrollHeight > descritpionEl.clientHeight)
-    {
-      const expandBtnTmpl = '<i class="task__arrow fa-solid fa-chevron-down"></i>';
-      thisTaskID.insertAdjacentHTML('beforeend',expandBtnTmpl)
-      const expandBtnEl = thisTaskID.querySelector('.task__arrow');
-      expandBtnEl.addEventListener('click', function(){
-        thisTaskID.classList.toggle('task--expanded');
-        expandBtnEl.classList.toggle('task__arrow--rotate')
-      })
+    if (descritpionEl.scrollHeight > descritpionEl.clientHeight) {
+      const expandBtnTmpl =
+        '<i class="task__arrow fa-solid fa-chevron-down"></i>';
+      thisTaskID.insertAdjacentHTML("beforeend", expandBtnTmpl);
+      const expandBtnEl = thisTaskID.querySelector(".task__arrow");
+      expandBtnEl.addEventListener("click", function () {
+        thisTaskID.classList.toggle("task--expanded");
+        expandBtnEl.classList.toggle("task__arrow--rotate");
+      });
     }
 
-    editBtn.addEventListener('click',function(){
-      
-      editBtn.classList.toggle('task__edit--active')
-      const editableElements ={
-        title: thisTaskID.querySelector('.task__title'),
-        description: thisTaskID.querySelector('.task__description'),
-        date: thisTaskID.querySelector('.task__date'),
-      }
+    editBtn.addEventListener("click", function () {
+      editBtn.classList.toggle("task__edit--active");
+      const editableElements = {
+        title: thisTaskID.querySelector(".task__title"),
+        description: thisTaskID.querySelector(".task__description"),
+        date: thisTaskID.querySelector(".task__date"),
+      };
       const editableElementsEl = [];
-      Object.values(editableElements).forEach(function(element,i){
+      Object.values(editableElements).forEach(function (element, i) {
         const elementClasses = element.classList;
         const elementData = element.textContent;
 
@@ -132,18 +132,18 @@ const refreshList = function () {
 
         let elementTemplate;
 
-        switch (i){
+        switch (i) {
           case 0:
-            elementTemplate = `<input class="${elementClasses}" type="text">`;
+            elementTemplate = `<input class="${elementClasses}" type="text" maxlength="9">`;
             break;
           case 1:
-            elementTemplate = `<textarea class="${elementClasses}"><textarea>`;
+            elementTemplate = `<textarea class="${elementClasses}"></textarea>`;
             break;
           case 2:
             elementTemplate = `<input class="${elementClasses}" type="date">`;
             break;
         }
-        thisTaskID.insertAdjacentHTML('beforeend',elementTemplate);
+        thisTaskID.insertAdjacentHTML("beforeend", elementTemplate);
         const taskInput = thisTaskID.querySelector(`.${elementClasses[0]}`);
         taskInput.value = elementData.trim();
         editableElementsEl.push(taskInput);
@@ -152,26 +152,31 @@ const refreshList = function () {
 
       const saveEditBtn = '<i class="fa-solid fa-floppy-disk task__edit"></i>';
       const notSaveEditBtn = `<i class="fa-solid fa-x task__edit"></i>`;
-      thisTaskID.insertAdjacentHTML("beforeend",saveEditBtn);
-      thisTaskID.insertAdjacentHTML("beforeend",notSaveEditBtn);
-      const saveEditBtnEl = thisTaskID.querySelector('.fa-floppy-disk');
-      const notSaveEditBtnEl = thisTaskID.querySelector('.fa-x');
+      thisTaskID.insertAdjacentHTML("beforeend", saveEditBtn);
+      thisTaskID.insertAdjacentHTML("beforeend", notSaveEditBtn);
+      const saveEditBtnEl = thisTaskID.querySelector(".fa-floppy-disk");
+      const notSaveEditBtnEl = thisTaskID.querySelector(".fa-x");
 
-      notSaveEditBtnEl.addEventListener('click',function(){
+      notSaveEditBtnEl.addEventListener("click", function () {
         refreshList();
       });
-      saveEditBtnEl.addEventListener('click',function() {
-         const indexOfTask = toDoList.findIndex(element => element.id === task.id);
-         
-         toDoList[indexOfTask] = 
-         {
-          name: thisTaskID.querySelector('.task__title').value.trim(),
-          description: thisTaskID.querySelector('.task__description').value.trim(),
-          date: thisTaskID.querySelector('.task__date').value && new Date(thisTaskID.querySelector('.task__date').value),
+      saveEditBtnEl.addEventListener("click", function () {
+        const indexOfTask = toDoList.findIndex(
+          (element) => element.id === task.id
+        );
+
+        toDoList[indexOfTask] = {
+          name: thisTaskID.querySelector(".task__title").value.trim(),
+          description: thisTaskID
+            .querySelector(".task__description")
+            .value.trim(),
+          date:
+            thisTaskID.querySelector(".task__date").value &&
+            new Date(thisTaskID.querySelector(".task__date").value),
           id: GenerateID(),
         };
         refreshList();
-       });
+      });
     });
 
     delButton.addEventListener("click", function () {
@@ -179,7 +184,6 @@ const refreshList = function () {
       toDoList.splice(taskIndex, 1);
       thisTaskID.remove();
     });
-
   });
 };
 
@@ -220,15 +224,10 @@ const checkInputCorrectness = function (...tocheck) {
   return isLengthCorrect ? true : false;
 };
 const removeAllWarnings = function () {
-  document .querySelectorAll(".event-warning")?.forEach((warning) => warning.remove());
+  document
+    .querySelectorAll(".event-warning")
+    ?.forEach((warning) => warning.remove());
 };
-
-
-
-
-
-
-
 
 const addToList = function () {
   removeAllWarnings();
